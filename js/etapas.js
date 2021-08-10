ffe604ee57a030e8cba75b7109890714beb5defc
yan <yanramos111@gmail.com>
2021-08-10 16:35:45 -0300

    CONFIRMA OK

---
+++ js/etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Lucas Carvalho',
+                partido: '420',
+                fotos: [
+                    {url: 'imgs/lucas.jpeg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '12312',
+                nome: 'Dough',
+                partido: 'Cherapó',
+                fotos: [
+                    {url: 'imgs/dough.jpeg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Ciclano',
+                partido: '420',
+                fotos: [
+                    {url: 'imgs/1.jpeg', legenda: 'Prefeito'},
+                    {url: 'imgs/1_vice.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Zulano',
+                partido: '421',
+                fotos: [
+                    {url: 'imgs/2.jpeg', legenda: 'Prefeito'},
+                    {url: 'imgs/2_vice.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,89 @@
+var vaga = document.querySelector('.d-1-1 span');
+var cargo = document.querySelector('.d-1-2 span');
+var descricao = document.querySelector('.d-1-4');
+
+
+var aviso = document.querySelector('.d-2');
+var lateral = document.querySelector('.d-1-right');
+var numeros = document.querySelector('.d-1-3');
+
+
+var numero = '';
+var etapaAtual = 0;
+
+function Comecaretapa(){
+    let etapa = etapas[etapaAtual];
+
+    let numeroHTML = '';
+
+    for(let i=0; i<etapa.numeros;i++){
+        if(i === 0){
+            numeroHTML += '<div class="numero pisca"></div>';
+        } else{
+            numeroHTML += '<div class="numero"></div>';
+        }
+    }
+    vaga.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML='';
+    aviso.style.display= 'none';
+    lateral.innerHTML='';
+    numeros.innerHTML = numeroHTML;
+}
+
+function atualizatela(){
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero == numero){
+            return true;
+        }else{
+            return false;
+        }
+    });
+    if(candidato.length > 0){
+        candidato = candidato[0];
+        vaga.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+        let fotosHTML = '';
+        for(var i in candidato.fotos){
+            fotosHTML += `<div class="d-1-image">
+                          <img src="${candidato.fotos[i].url}" alt=""/>
+                          ${candidato.fotos[i].legenda}
+                          </div>`;
+        }
+        lateral.innerHTML = fotosHTML;
+    }else{
+        vaga.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = <div class="aviso--grande">VOTO NULO</div>;
+    }
+
+    console.log("Candidato", candidato);
+}
+
+function clicou (n){
+    var num = document.querySelector('.numero.pisca');
+    if(num !== null){
+        num.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        num.classList.remove('pisca');
+        if(num.nextElementSibling !== null){
+        num.nextElementSibling.classList.add('pisca');
+        }else{
+            atualizatela();
+        }
+    }
+}
+function branco(){
+    alert("Clicou no branco!");
+}
+function corrige(){
+    alert("Clicou no corrige!");
+}
+function confirma(){
+    alert("Clicou no confirma!");
+}
+
+Comecaretapa();
